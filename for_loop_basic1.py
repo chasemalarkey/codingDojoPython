f1a9d67b0d228f9c6228cb118e06f8e182e1b447
Chase Malarkey <127543574+chasemalarkey@users.noreply.github.com>
2023-12-22 18:06:35 -0500

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,32 @@
+# for i in range(0,151):
+#     print(i)
+
+# for i in range(5,1001,5):
+#     print(i)
+
+# for i in range(1,101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(i)
+
+# sum = 0
+# for i in range(0, 500001):
+#     if i % 2 == 1:
+#         sum += i
+#     else:
+#         continue
+# print(sum)
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+low_num = 2
+high_num = 9
+mult = 3
+
+for i in range(low_num, high_num+1):
+    if i % mult == 0:
+        print(i)
